e3c46a9f408044c48e2b648640eb365d3b18da79
Elvisjan <ilgamilgam@gmail.com>
2022-06-10 00:48:23 +0500

    Добавлены обработчики ошибок

--- src/routes/bloggers-router.ts
+++ src/routes/bloggers-router.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response, Router } from "express"
 
-export const bloggersRouter =Router({})
+export const bloggersRouter = Router({})
 
 let bloggers = [
   {
@@ -34,88 +34,121 @@ let bloggers = [
     "youtubeUrl": "gxcvxcfasdxzc"
   }
 ]
+type Error = {
+  message: string
+  field: string
+}
+const errorHandler = ( errors: Error[],message: string, field: string) => {
+  const error: Error = {
+      message,
+      field
+  }
+  errors.push(error)
+}
+const sendError = (res: Response, errors: Error[], status: number) => {
+  const payload = {
+    errors,
+    resultCode: 1
+  }
+  res.status(status).send(payload)
+}
 bloggersRouter.get('/', (req: Request, res: Response) => {
   res.status(200).send(bloggers)
 })
 bloggersRouter.post('/', (req: Request, res: Response) => {
-  if(!req.body.name|| req.body.name.length > 40) {
-    res.status(400).send({ errorsMessages: [{ message: "string", field: "name" }], resultCode: 1 })
+  const uriRegexp = new RegExp("^(http[s]?:\\/\\/(www\\.)?|ftp:\\/\\/(www\\.)?|www\\.){1}([0-9A-Za-z-\\.@:%_\+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?")
+  const errors: Error[] = []
+  const {body} = req
+  if (!body.name || body.name.length > 15) {
+    errorHandler(errors,'name is not valid','name')
   }
-  const newBlogger = {
+  if(!uriRegexp.test(body.youtubeUrl) || !body.youtubeUrl || body.youtubeUrl.length > 100 ) errorHandler(errors, 'uri is not valid','youtubeUrl') 
+  if(errors.length > 0) sendError(res, errors, 404)
+  else {
+    const newBlogger = {
       id: +(new Date()),
       name: req.body.name,
       youtubeUrl: 'it-incubator.eu'
+    }
+    bloggers.push(newBlogger)
+    res.status(201).send(newBlogger)
   }
-  bloggers.push(newBlogger)
-  res.status(201).send(newBlogger)
 })
 bloggersRouter.get('/:bloggerId', (req: Request, res: Response) => {
   const id = +req.params.bloggerId;
-  const currentVideo = bloggers.find(v=>v.id===id)
-  if(!id) {
+  const currentVideo = bloggers.find(v => v.id === id)
+  if (!id) {
     res.sendStatus(404)
   }
-  if(!currentVideo) {
+  if (!currentVideo) {
     res.sendStatus(404)
   }
   res.send(currentVideo)
 })
-bloggersRouter.delete('/:id',(req: Request, res: Response)=>{
+bloggersRouter.delete('/:id', (req: Request, res: Response) => {
   const id = +req.params.id;
-  if(!id) {
+  if (!id) {
     res.sendStatus(404)
   }
-  if(!bloggers.map(v=>v.id).includes(id)) {
+  if (!bloggers.map(v => v.id).includes(id)) {
     res.sendStatus(404)
   }
-  bloggers=bloggers.filter(v=>v.id!==id)
+  bloggers = bloggers.filter(v => v.id !== id)
   res.sendStatus(204)
- })
- bloggersRouter.put('/:id',(req: Request, res: Response) => {
+})
+bloggersRouter.put('/:id', (req: Request, res: Response) => {
+  const uriRegexp = new RegExp("^(http[s]?:\\/\\/(www\\.)?|ftp:\\/\\/(www\\.)?|www\\.){1}([0-9A-Za-z-\\.@:%_\+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?")
+  const errors: Error[] = []
+  const {body} = req
+  if (!body.name || body.name.length > 15) {
+    errorHandler(errors,'name is not valid','name')
+  }
+  if(!uriRegexp.test(body.youtubeUrl) || !body.youtubeUrl || body.youtubeUrl.length > 100 ) errorHandler(errors, 'uri is not valid','youtubeUrl') 
+  if(errors.length > 0) sendError(res, errors, 404)
   const id = +req.params.id
   const newName = req.body.name
   const newYoutubeUrl = req.body.youtubeUrl
-  const blogger = bloggers.find(v=>v.id === id)
-  if(!id) {
+  const blogger = bloggers.find(v => v.id === id)
+  if (!id) {
     res.sendStatus(404)
   }
-  if(blogger && newName.length <= 40 ){
+  if (blogger && newName.length <= 40) {
     blogger.name = newName
     blogger.youtubeUrl = newYoutubeUrl
-      res.status(204).send(blogger)
-      return
+    res.status(204).send(blogger)
+    return
   }
-  if(blogger && newName.length > 40 ){
-      res.status(400).send({
-          "errorsMessages": [
-              {
-                  "message": "string",
-                  "field": "string"
-              }
-          ],
-          "resultCode": 0
-      })
-      return
-  }
-  if(!bloggers.map(v=>v.id).includes(id)) {
-    res.sendStatus(404)
-  }
-  if(!blogger){
-      res.status(204).send('No Content')
-      return;
-  }
-  res.status(404).send({
+  if (blogger && newName.length > 40) {
+    res.status(400).send({
       "errorsMessages": [
-          {
-              "message": "string",
-              "field": "string"
-          }
+        {
+          "message": "string",
+          "field": "string"
+        }
       ],
       "resultCode": 0
+    })
+    return
+  }
+  if (!bloggers.map(v => v.id).includes(id)) {
+    res.sendStatus(404)
+  }
+  if (!blogger) {
+    res.status(204).send('No Content')
+    return;
+  }
+  res.status(404).send({
+    "errorsMessages": [
+      {
+        "message": "string",
+        "field": "string"
+      }
+    ],
+    "resultCode": 0
   })
   return;
 })
-bloggersRouter.delete('/',(req: Request, res: Response)=>{
+bloggersRouter.delete('/', (req: Request, res: Response) => {
   bloggers = []
   res.sendStatus(204)
- })
+})
