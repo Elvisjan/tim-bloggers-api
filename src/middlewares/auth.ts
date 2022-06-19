ac46cf98554c6eb07b49afa3c8ddfce45a302a02
Elvisjan <ilgamilgam@gmail.com>
2022-06-19 19:11:46 +0500

    Добавлен пакет base-64 и переделана авторизация

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,11 +1,22 @@
 import { NextFunction, Request, Response } from "express"
-
+import base64 from "base-64"
+// express basic auth middleware
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  const basicAuth = req.headers.authorization ?? ''
-  const [,credentials] = basicAuth?.split(' ')
-  const [login,password] = credentials?.split(':')
-  if(!(login||password) || login !== 'admin' || password!=='qwerty') {
-    res.status(401)
+  try {
+    if (req.headers.authorization) {
+      const [type, credentials] = req.headers.authorization.split(" ")
+      if (type === "Basic" && (base64.decode(credentials) === "admin:qwerty")) {
+        next()
+      }
+      else {
+        res.status(401).send("Unauthorized")
+      }
+    }
+    else {
+      res.status(401).send("Unauthorized")
+    }
   }
-  next()
+  catch (err) {
+    res.status(500).send(err)
+  }
 }
